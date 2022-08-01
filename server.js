57156c44f86ef5a4bc5c1533f5590754a97325e8
Abdi Yusuf <ayusufadan@gmail.com>
2022-08-01 08:50:05 -0500

    6weekassessment

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const {bots, playerRecord} = require('./data')
+const {shuffleArray} = require('./utils')
+
+app.use(express.json())
+
+//endpoint for Heroku to access HTML file
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+// file path for the html file:
+// C:\Users\ayusu\Desktop\DevMountain\Assessments\Week6-Assessment\assessment-qa-devops\public\index.html
+
+
+app.get('/api/robots', (req, res) => {
+    try {
+        res.status(200).send(botsArr)
+    } catch (error) {
+        console.log('ERROR GETTING BOTS', error)
+        res.sendStatus(400)
+    }
+})
+
+app.get('/api/robots/five', (req, res) => {
+    try {
+        let shuffled = shuffleArray(bots)
+        let choices = shuffled.slice(0, 5)
+        let compDuo = shuffled.slice(6, 8)
+        res.status(200).send({choices, compDuo})
+    } catch (error) {
+        console.log('ERROR GETTING FIVE BOTS', error)
+        res.sendStatus(400)
+    }
+})
+
+app.post('/api/duel', (req, res) => {
+    try {
+        // getting the duos from the front end
+        let {compDuo, playerDuo} = req.body
+
+        // adding up the computer player's total health and attack damage
+        let compHealth = compDuo[0].health + compDuo[1].health
+        let compAttack = compDuo[0].attacks[0].damage + compDuo[0].attacks[1].damage + compDuo[1].attacks[0].damage + compDuo[1].attacks[1].damage
+        
+        // adding up the player's total health and attack damage
+        let playerHealth = playerDuo[0].health + playerDuo[1].health
+        let playerAttack = playerDuo[0].attacks[0].damage + playerDuo[0].attacks[1].damage + playerDuo[1].attacks[0].damage + playerDuo[1].attacks[1].damage
+        
+        // calculating how much health is left after the attacks on each other
+        let compHealthAfterAttack = compHealth - playerAttack
+        let playerHealthAfterAttack = playerHealth - compAttack
+
+        // comparing the total health to determine a winner
+        if (compHealthAfterAttack > playerHealthAfterAttack) {
+            playerRecord.losses++
+            res.status(200).send('You lost!')
+        } else {
+            playerRecord.losses++
+            res.status(200).send('You won!')
+        }
+    } catch (error) {
+        console.log('ERROR DUELING', error)
+        res.sendStatus(400)
+    }
+})
+
+app.get('/api/player', (req, res) => {
+    try {
+        res.status(200).send(playerRecord)
+    } catch (error) {
+        console.log('ERROR GETTING PLAYER STATS', error)
+        res.sendStatus(400)
+    }
+})
+
+//Heroku has its own ENV file where it stores their port numbers
+//port numbers are randomized and it will be different everytime we try to access the port
+//we use this line to get into that port
+// also we set up a local port at 3000 for local testing!
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
